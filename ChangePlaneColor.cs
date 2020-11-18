dc10fcbcb8bb7182f75fa027b29adfeb73719f98
Knarf3877 <70541903+Knarf3877@users.noreply.github.com>
2020-11-18 16:57:08 -0600

    A basic button to change an objects color

---
+++ ChangePlaneColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlaneColor : MonoBehaviour
+{
+    public GameObject plane;
+    private MeshRenderer myRenderer;
+
+    private void Start()
+    {
+        Debug.Log("This button is working");
+    }
+
+    public void OnChangeColor ()
+    {
+        myRenderer = plane.GetComponent<MeshRenderer>();
+        myRenderer.enabled = !myRenderer.enabled;
+    }
+}
