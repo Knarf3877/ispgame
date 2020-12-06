439ad274db8ebe2025ef69c46387cbeed9fdaf32
Knarf3877 <70541903+Knarf3877@users.noreply.github.com>
2020-12-05 19:47:33 -0600

    Quit Game Feature
    Allows a button to, on click, close the application

---
+++ QuitGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+
+    public void Start()
+    {
+        Debug.Log("Button Working");
+    }
+    public void ExitGame ()
+    {
+        Application.Quit();
+        Debug.Log("game closed");
+        Application.Quit();
+    }
+
+}
+
