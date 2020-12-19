d3714e1f2a89bbd4be30cec1f7e1428d1699e87f
Knarf3877 <70541903+Knarf3877@users.noreply.github.com>
2020-12-19 16:50:26 -0600

    Main Menu

---
+++ MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject options;
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        options.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("game quit");
+    }
+
+    public void ToggleOptionsMenu()
+    {
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        options.SetActive(!options.activeSelf);
+    }
+}
